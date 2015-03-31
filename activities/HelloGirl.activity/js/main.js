c87c7b514c5c84efd9c72119c0cabe2a7e215e78
castelliam <castelliam@g.cofc.edu>
2015-03-31 09:31:16 -0400

    Newest Update without JavaScript functionality

--- activities/HelloGirl.activity/js/main.js
+++ activities/HelloGirl.activity/js/main.js
@@ -1,3 +1,12 @@
 function changeImage(a) {
+	//alert('Inside changeImage');
     document.getElementById("doll_image").src=a;
 }
+
+/*function addPants(a) {
+	alert('Inside Add Pants');
+    //document.getElementById("doll_image").innerHTML = '<img src="images/pant1.png"/>;'>;
+    var img = document.createElement('img')
+	img.src = a;
+    document.getElementById("doll_container").appendChild(img);
+}*/
