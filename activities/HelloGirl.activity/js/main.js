2707f281b1511cf1b04f017db016ea804db7e93a
castelliam <castelliam@g.cofc.edu>
2015-04-09 08:43:48 -0400

    Draggable functionality going into div

--- activities/HelloGirl.activity/js/main.js
+++ activities/HelloGirl.activity/js/main.js
@@ -3,6 +3,20 @@ function changeImage(a) {
     document.getElementById("doll_image").src=a;
 }
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
 /*function addPants(a) {
 	alert('Inside Add Pants');
     //document.getElementById("doll_image").innerHTML = '<img src="images/pant1.png"/>;'>;
