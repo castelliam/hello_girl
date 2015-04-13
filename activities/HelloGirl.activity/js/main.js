ce1ad92f3b4a54a5e007fe1f0ca09329f31b8acf
castelliam <castelliam@g.cofc.edu>
2015-04-12 22:57:36 -0400

    Added more items/changing code functionality/draggable functionality improved (but not finished)

--- activities/HelloGirl.activity/js/main.js
+++ activities/HelloGirl.activity/js/main.js
@@ -1,26 +1,7 @@
 function changeImage(a) {
-	//alert('Inside changeImage');
     document.getElementById("doll_image").src=a;
 }
 
-function allowDrop(ev) {
-    ev.preventDefault();
+function changeWord(idElement, id){
+    document.getElementById(id).innerHTML=idElement;
 }
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
-
-/*function addPants(a) {
-	alert('Inside Add Pants');
-    //document.getElementById("doll_image").innerHTML = '<img src="images/pant1.png"/>;'>;
-    var img = document.createElement('img')
-	img.src = a;
-    document.getElementById("doll_container").appendChild(img);
-}*/
